abad38dc675eb991b39d126cbfb8701fd74976bc
rajkumar-wrk <raj.officialwrkmail@gmail.com>
2025-06-05 13:04:36 +0530

    json report path modified

--- src/test/java/com/testrunner/TestRunner.java
+++ src/test/java/com/testrunner/TestRunner.java
@@ -11,28 +11,14 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions( dryRun = false, glue="com.stepdefinations",features ="src\\test\\resources\\Features",plugin = {"pretty","json:E:\\class\\maven\\Api_Cucumber\\target\\jsonReports\\api.json"})
+@CucumberOptions(dryRun = false, glue = "com.stepdefinations", features = "src\\test\\resources\\Features", plugin = {
+		"pretty", "json:target\\jsonReports\\api.json" })
 public class TestRunner {
-	
-	
+
 	@AfterClass
 	public static void afterClass() {
-		Reporting.jvmReport(new File(System.getProperty("user.dir"))+"\\\\target\\\\jsonReports\\\\api.json");
+		Reporting.jvmReport(new File(System.getProperty("user.dir")) + "\\\\target\\\\jsonReports\\\\api.json");
 
 	}
-	
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
